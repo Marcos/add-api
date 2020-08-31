48940effa00a0771195eb184d3884619d433145f
Marcos <marcos.ferreira@creditas.com>
2020-08-31 20:28:52 +0200

    Adding project skeleton

---
+++ src/main/kotlin/com/add/addapi/AddApiApplication.kt
@@ -0,0 +1,11 @@
+package com.add.addapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AddApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AddApiApplication>(*args)
+}
